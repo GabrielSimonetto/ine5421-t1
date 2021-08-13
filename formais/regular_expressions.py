61d97969e6c653ad73a530d7e1af2fd4092c828e
Gabriel Simonetto <gabrielfranksimonetto@gmail.com>
2021-08-12 22:37:47 -0300

    Remove comments

--- formais/regular_expressions.py
+++ formais/regular_expressions.py
@@ -33,36 +33,6 @@ class RegularExpression:
 				formatted.append(f'{left}:{right}\n')
 			writer.write("".join(formatted))
 
-    # # TODO: Rename to RE
-    # # TODO: try to put this stuff into regular_expressions.py
-    # def ER_to_AFD(self):
-    #     ER = ER.regex['er'] + "#"
-    #     regex = format_er(ER)
-    #     rpn_regex = convertRPN(regex)
-    #     prefixed = post_to_pre(rpn_regex)
-    #     new = construct_tree(prefixed)
-    #     parseLeaves(new)
-    #     first_and_lastPos(new)
-    #     print("Arvore Sintática:")
-    #     print(new.__repr__())
-    #     def show(symbol):
-    #         print(symbol.value, "First Pos:", symbol.fPos, "Last Pos:", symbol.lPos)
-    #         for c in symbol.children:
-    #             show(c)
-    #     print("First e Last Pos:")
-    #     show(new)
-    #     followp = follow(new)
-    #     table = PrettyTable()
-    #     table.add_column("n", list(followp.keys()))
-    #     table.add_column("followpos", list(followp.values()))
-    #     print("\n")
-    #     print("Followpos:")
-    #     print(table)
-    #     AF = createAFD(new, followp)
-    #     print("\n")
-    #     print("Autômato:")
-    #     AF.show()
-
 
 class Node():
 	def __init__(self, span, value, children, fPos, lPos):
